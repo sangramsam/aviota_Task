30a5162c5766b84c69fbf922b17e2b79bff7a92d
sangramsam <singh.sangram56@gmail.com>
2017-08-25 02:54:52 +0530

    implent api

--- config/connect.js
+++ config/connect.js
@@ -1,3 +1,4 @@
 module.exports={
-    database:"mongodb://localhost:27017/aviota"
+    //database:"mongodb://localhost:27017/aviota"
+    database:"mongodb://<avoit>:<12345>@ds159033.mlab.com:59033/heroku_zffhck59"
 }
